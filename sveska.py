bd23b295619ac775ac7067338a1f1aff906ce6f1
Max <kollg.maks@gmail.com>
2021-11-12 19:43:55 +0600

    Добавлена ссылка на мой GitHub

--- sveska.py
+++ sveska.py
@@ -65,17 +65,4 @@ create_app()
 
 
 
-#from PyQt5 import uic
-#from PyQt5.QtWidgets import QApplication 
-
-
-#Form, Window = uic.loadUiType(".ui")
-
 #
-
-#app = QApplication([])
-#window = Window()
-#form = Form()
-#form.setupUi(window)
-#window.show()
-#app.exec_()
